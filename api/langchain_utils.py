6f3f1398ec90b2b13d7d8040a97b8dc57b385f34
TBA5854 <adhavan5030@gmail.com>
2025-11-09 19:50:50 +0530

    fix : fixed all paths

--- api/langchain_utils.py
+++ api/langchain_utils.py
@@ -6,8 +6,7 @@ from langchain.chains import create_history_aware_retriever, create_retrieval_ch
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from typing import List
 from langchain_core.documents import Document
-import os
-from chroma_utils import vectorstore
+from .chroma_utils import vectorstore
 retriever = vectorstore.as_retriever(search_kwargs={"k": 2})
 
 output_parser = StrOutputParser()
